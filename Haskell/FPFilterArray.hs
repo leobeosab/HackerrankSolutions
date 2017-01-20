d11d1c2cbeee777ec0e7bab8b2fb911f314cf78b
Ryan Wise <ryan.wise@wiseweb-design.com>
2017-01-19 21:46:19 -0500

    FPFilterArray haskell solution

---
+++ Haskell/FPFilterArray.hs
@@ -0,0 +1,11 @@
+--Ryan Wise 1/19/2017
+f :: Int -> [Int] -> [Int]
+f n arr = [ x | x <- arr, x < n ]
+
+-- The Input/Output section. You do not need to change or modify this part
+main = do
+    n <- readLn :: IO Int
+    inputdata <- getContents
+    let
+        numbers = map read (lines inputdata) :: [Int]
+    putStrLn . unlines $ (map show . f n) numbers
